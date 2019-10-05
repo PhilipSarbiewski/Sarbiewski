1fc063f9e23c1d077716c7575de658686faa834f
PhilipSarbiewski <55675720+PhilipSarbiewski@users.noreply.github.com>
2019-10-04 21:17:44 -0400

    Create multiply.cpp

---
+++ BigInt/multiply.cpp
@@ -0,0 +1,25 @@
+#include <iostream>
+#include <fstream>
+#include <cstdlib>
+#include "bigint.hpp"
+
+
+int main() {
+  std::ifstream in("data1-2.txt");    // Define stream for input
+  if(!in) {                           // Make sure it opened correctly.
+    std::cout << "Could not open data1-1.txt, exiting." << std::endl;
+    return 1;
+  }
+  bigint lhs;
+  bigint rhs;
+  bigint sum;
+  while (in >> lhs && in >> rhs){
+    std::cout << "Left = " << lhs << std::endl;
+    std::cout << "Right = " << rhs << std::endl;
+    sum = lhs * rhs;
+    std::cout << "Product = " << sum << std::endl;
+  }
+
+  in.close();
+  return 0;
+}
